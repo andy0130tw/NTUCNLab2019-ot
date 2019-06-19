c94527a59de4848b839c3d61da280da1c72e1cc5
Andy Pan <andypan@ntuosc.org>
2019-06-20 00:40:54 +0800

    Polish; add rev

--- src/js/index.js
+++ src/js/index.js
@@ -12,139 +12,131 @@ const { AjaxAdapter,
         WrappedOperation } = ot
 
 
-const client = new ot.Client(0)
-const peer = new ot.Client(0)
-const server = new MockedServer('')
-console.log(client)
-console.log(server)
-
-const cm = CodeMirror.fromTextArea(document.getElementById('editor'), {
-  lineNumbers: true,
-})
-cm.setSize(null, (6 * 1.6) + 'em')
-
-// will be removed when demo
-const cmDebug = CodeMirror.fromTextArea(document.getElementById('editor-debug'), {
-  lineNumbers: true
-})
+function createSpan(text) {
+  const el = document.createElement('span')
+  el.textContent = text
+  return el
+}
 
 const changesPanel = document.getElementById('changes-panel')
 
 const opToHtml = operation => {
   const { meta } = operation
   const { baseLength, targetLength, ops } = operation.wrapped
-  const opText = ops.map(opDesc => {
+  const opEl = ops.map(opDesc => {
     if (typeof opDesc == 'number') {
       if (opDesc > 0) {
-        return `Retain(${opDesc})`
+        const el = createSpan(`Retain(${opDesc})`)
+        el.style.color = 'gray'
+        return el
       }
-      return `Delete(${-opDesc})`
+      const el = createSpan(`Delete(${-opDesc})`)
+      el.style.color = 'red'
+      return el
     }
     if (typeof opDesc == 'string') {
       const dispStr = opDesc.replace(/\n/g, '\\n')
-      return `Insert("${dispStr}")`
+      const el = createSpan(`Insert("${dispStr}")`)
+      el.style.color = 'blue'
+      return el
     }
     console.warn('Unreachable... :(')
     return null
   })
 
-  const el = document.createElement('div')
-  el.textContent = `${meta.creator}: [${baseLength} -> ${targetLength}]` + opText
-  return el
+  const elContainer = document.createElement('div')
+  elContainer.innerHTML = `<strong><code>${meta.creator}</code></strong>`
+  elContainer.append(`: [${baseLength} → ${targetLength}] `)
+  opEl.forEach((el, i) => {
+    if (i > 0)
+      elContainer.append(', ')
+    elContainer.appendChild(el)
+  })
+  return elContainer
 }
 
-const opBuffer = []
-const networkChannel = new EventEmitter()
-const peerChannel = new EventEmitter()
-
-const state = {
-  isIdle: true
-}
-const statePeer = {
-  isIdle: true
-}
-
-function sendToServer(operation) {
+function sendToServer(revision, operation) {
+  peers.forEach(client => {
+    if (client._state.isMaster) {
+      document.getElementById('mainRevision').textContent = revision
+    }
+  })
   setTimeout(() => {
-    [networkChannel, peerChannel].forEach(ch => {
-      ch.emit('receive', operation)
+    peers.forEach(client => {
+      client.channel.emit('receive', operation)
     })
   }, 1000)
 }
 
-client.sendOperation = function (revision, operation) {
-  console.log('sendOp A', revision, operation)
-  const resp = server.receiveOperation(revision, operation)
+function setupClient(name, cm, state, initRevision) {
+  const client = new Client(initRevision)
+  const channel = new EventEmitter()
 
-  changesPanel.prepend(opToHtml(operation))
-  sendToServer(resp)
+  client._state = state
+  client.channel = channel
+
+  client.sendOperation = function(revision, operation) {
+    console.log('send op', name, revision, operation)
+    const resp = server.receiveOperation(revision, operation)
+    changesPanel.prepend(opToHtml(operation))
+    sendToServer(revision, resp)
+  }
+
+  client.applyOperation = function(operation) {
+    console.log('apply op', name, operation)
+    state.isIdle = false
+    CodeMirrorAdapter.applyOperationToCodeMirror(operation.wrapped, cm)
+    state.isIdle = true
+  }
+
+  cm.on('changes', function(cm, changes) {
+    if (!state.isIdle) return
+    const operationUnwrapped = CodeMirrorAdapter.operationFromCodeMirrorChanges(changes, cm)[0]
+    const operation = new WrappedOperation(operationUnwrapped, {
+      creator: name,
+      id: 'operation' + Math.random() * 1000,
+    });
+    client.applyClient(operation)
+  })
+
+  channel.on('receive', operation => {
+    setTimeout(() => {
+      if (operation.meta.creator == name) {
+        return client.serverAck()
+      } else {
+        return client.applyServer(operation)
+      }
+    }, 2000)
+  })
+
+  return client
 }
 
-client.applyOperation = function (operation) {
-  console.log('applyOp A', operation)
-  state.isIdle = false
-  CodeMirrorAdapter.applyOperationToCodeMirror(operation.wrapped, cm)
-  state.isIdle = true
-}
+const peerNames = 'ABC'.split('')
+const initDoc = 'asd'
 
+const server = new MockedServer(initDoc)
+const peers = []
 
-peer.sendOperation = function (revision, operation) {
-  console.log('sendOp B', revision, operation)
-  const resp = server.receiveOperation(revision, operation)
+const [sA, sB, sC] = peerNames.map(() => ({
+  isIdle: true,
+}))
 
-  changesPanel.prepend(opToHtml(operation))
-  sendToServer(resp)
-}
-
-peer.applyOperation = function (operation) {
-  console.log('applyOp B', operation)
-  statePeer.isIdle = false
-  CodeMirrorAdapter.applyOperationToCodeMirror(operation.wrapped, cmDebug)
-  statePeer.isIdle = true
-}
-
-// networkChannel.on('send', operation => {
-//   changesPanel.prepend(opToHtml(operation))
-//   peerChannel.emit('receive', operation)
-// })
-
-networkChannel.on('receive', operation => {
-  // statePeer.isIdle = false
-  // CodeMirrorAdapter.applyOperationToCodeMirror(operation.wrapped, cm)
-  // statePeer.isIdle = true
-  setTimeout(() => {
-    if (operation.meta.creator == 'A') {
-      return client.serverAck();
-    } else {
-      return client.applyServer(operation);
-    }
-  }, 2000)
+const [cmA, cmB, cmC] = peerNames.map(c => {
+  const textarea = document.getElementById('editor' + c)
+  textarea.value = initDoc
+  const cm = CodeMirror.fromTextArea(textarea, {
+    lineNumbers: true,
+  })
+  cm.setSize(null, (6 * 1.6) + 'em')
+  return cm
 })
 
-peerChannel.on('receive', operation => {
-  setTimeout(() => {
-    if (operation.meta.creator == 'B') {
-      return peer.serverAck();
-    } else {
-      return peer.applyServer(operation);
-    }
-  }, 2000)
-})
+sA.isMaster = true
 
-cm.on('changes', function(cm, changes) {
-  if (!state.isIdle) return
-  const operation = new WrappedOperation(CodeMirrorAdapter.operationFromCodeMirrorChanges(changes, cm)[0], {
-    creator: 'A',
-    id: 'operation' + Math.random() * 1000,
-  });
-  client.applyClient(operation)
-})
 
-cmDebug.on('changes', function(cm, changes) {
-  if (!statePeer.isIdle) return
-  const operation = new WrappedOperation(CodeMirrorAdapter.operationFromCodeMirrorChanges(changes, cm)[0], {
-    creator: 'B',
-    id: 'operationxxx',
-  });
-  peer.applyClient(operation)
-})
+const clientA = setupClient('A', cmA, sA, 0)
+const clientB = setupClient('B', cmB, sB, 0)
+const clientC = setupClient('C', cmC, sC, 0)
+
+peers.push(clientA, clientB, clientC)
